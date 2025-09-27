5cd37d3767ea1aa54dde48bcbebaa57e28f511ea
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-09-26 20:19:01 -0500

    Created additional functions for retrieving employees.

--- src/api/v1/services/employeeService.ts
+++ src/api/v1/services/employeeService.ts
@@ -26,7 +26,8 @@ export const getEmployeeById = async (id: number): Promise<Employee | undefined>
  * @param employeeData - Only the fields needed to create an employee
  * @returns The created employee
  */
-export const createEmployee = async (employeeData: Omit<Employee, "id">): Promise<Employee> => {
+export const createEmployee = async (employeeData: Omit<Employee, "id">
+): Promise<Employee> => {
     const newEmployee: Employee = {
         id: Date.now(),
         name: employeeData.name,
@@ -69,7 +70,8 @@ export const updateEmployee = async (
  * @param id - The ID of the employee to delete
  * @returns The confirmation if the employee was deleted or undefined if not found
  */
-export const deleteEmployee = async (id: number): Promise<string | undefined> => {
+export const deleteEmployee = async (id: number
+): Promise<string | undefined> => {
     const index: number = employees.findIndex((employee: Employee) => employee.id === id);
 
     if (index === -1){
@@ -78,4 +80,40 @@ export const deleteEmployee = async (id: number): Promise<string | undefined> =>
 
     employees.splice(index, 1);
     return "Employee deleted successfully.";
+};
+
+/**
+ * Retrieves all employees from a branch
+ * @param branchId - The specified branch
+ * @returns An array of all the employees from that branch
+ */
+export const getAllEmployeesFromBranch = async (branchId: number
+): Promise<Employee[]> => {
+    const employeesFromBranch: Employee[] = [];
+
+    for (const employee of employees){
+        if (employee.branchId === branchId){
+            employeesFromBranch.push(structuredClone(employee));
+        }
+    }
+
+    return employeesFromBranch;
+};
+
+/**
+ * Retrieves all employees from a department
+ * @param department - The specified department
+ * @returns An array of all the employees from that department
+ */
+export const getAllEmployeesFromDepartment = async (department: string
+): Promise<Employee[]> => {
+    const employeesFromDepartment: Employee[] = [];
+
+    for (const employee of employees){
+        if (employee.department === department){
+            employeesFromDepartment.push(structuredClone(employee));
+        }
+    }
+
+    return employeesFromDepartment;
 };
