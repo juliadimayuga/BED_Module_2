4b5be72961faf49205791a30b1d7f79674a0f6bb
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-10-08 10:49:10 -0500

    Corrected validation for phone numbers.

--- src/api/v1/validators/branchValidators.ts
+++ src/api/v1/validators/branchValidators.ts
@@ -3,5 +3,5 @@ import Joi from "joi";
 export const createBranchSchema = Joi.object({
     name: Joi.string().min(2).max(40).required(),
     address: Joi.string().min(4).max(30).required(),
-    phone: Joi.string().length(10).regex(/^[0-9]{10}$/).required()
+    phone: Joi.string().regex(/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/).required()
 });
--- src/api/v1/validators/employeeValidators.ts
+++ src/api/v1/validators/employeeValidators.ts
@@ -5,6 +5,6 @@ export const createEmployeeSchema = Joi.object({
     position: Joi.string().required(),
     department: Joi.string().required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().length(10).regex(/^[0-9]{10}$/).required(),
+    phone: Joi.string().regex(/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/).required(),
     branchId: Joi.number().integer().required()
 });
