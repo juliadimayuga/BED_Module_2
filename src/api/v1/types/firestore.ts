0bf2ac9e62bda486a3d6f9b330af22f4000db6c6
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-10-10 20:38:22 -0500

    Updated data types.

--- src/api/v1/types/firestore.ts
+++ src/api/v1/types/firestore.ts
@@ -1,4 +1,12 @@
+import { FieldValue, Timestamp } from "firebase-admin/firestore";
+
 /**
  * Types used for data.
  */
-export type FirestoreDataTypes = string | number | object;
+export type FirestoreDataTypes =
+    | string
+    | number
+    | boolean
+    | null
+    | Timestamp
+    | FieldValue;
