31071ff597f8273159f6376f50fcd321b4b5bb08
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-11-13 20:29:55 -0600

    Added documentation for getAllBranches.

--- src/api/v1/routes/branchRoute.ts
+++ src/api/v1/routes/branchRoute.ts
@@ -10,6 +10,20 @@ import { validateCreateBranch } from "../middleware/validatorMiddleware";
 
 const router: Router = express.Router();
 
+/**
+ * @openapi
+ * /branches
+ *   get:
+ *     summary: Retrieve all branches
+ *     tags: [Branches]
+ *     responses:
+ *       '200':
+ *         description: Branches retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ */
 router.get("/branches", getAllBranches);
 router.get("/branches/:id", getBranchById);
 router.post("/branches", validateCreateBranch, createBranch);
